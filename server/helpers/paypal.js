8045e5b12898f665057ea5e1399f763fceabc60a
Chirag singh <chiragsingh8926@gmail.com>
2025-04-23 23:36:12 +0530

    add controllers for shop

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,12 +1,32 @@
 const paypal = require("paypal-rest-sdk");
+const dotenv = require("dotenv");
 
-import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config();
 
 paypal.configure({
-  mode: "",
+  mode: "sandbox", // or "live"
   client_id:  "",
   client_secret:  "",
 });
 
+// paypal.configure({
+//   mode: "sandbox", // or "live"
+//   client_id: process.env.PAYPAL_CLIENT_ID,
+//   client_secret: process.env.PAYPAL_CLIENT_SECRET,
+// });
+
 module.exports = paypal;
+
+
+// const paypal = require("paypal-rest-sdk");
+
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+// paypal.configure({
+//   mode: "",
+//   client_id:  "",
+//   client_secret:  "",
+// });
+
+// module.exports = paypal;
--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const adminOrderRouter = require("./routes/admin/order-routes");
 
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 
 // 
@@ -44,6 +50,13 @@ app.use(
   app.use("/api/auth", authRouter);
   app.use("/api/admin/products", adminProductsRouter);
   app.use("/api/admin/orders", adminOrderRouter);
-  
+
+  app.use("/api/shop/products", shopProductsRouter);
+  app.use("/api/shop/cart", shopCartRouter);
+  app.use("/api/shop/address", shopAddressRouter);
+  app.use("/api/shop/order", shopOrderRouter);
+  app.use("/api/shop/search", shopSearchRouter);
+  app.use("/api/shop/review", shopReviewRouter);
+
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
   
