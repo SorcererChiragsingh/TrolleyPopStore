cedaf0f3a99dd69f1bc5b0c57f9954643b3beea0
Chirag singh <chiragsingh8926@gmail.com>
2025-04-25 23:34:35 +0530

    Paypal config

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 paypal.configure({
   mode: "sandbox", // or "live"
-  client_id:  "",
-  client_secret:  "",
+  client_id:  "AWE7YBCcSlGdb20U3fV77tOpl5uilNHzMJw9s7aF8To1wMw3K2gFf5FgHj-Z8awI3xyGxltl-fCARbKV",
+  client_secret:  "ELfGO-QvP923WsH0ZwDM7NUmEZ47ribQ044YDOR1nBwiI9BNMxmned_FjUNVbPt_N9O18u9NzfJnt-KD",
 });
 
 // paypal.configure({
